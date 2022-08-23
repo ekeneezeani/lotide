c844672ce852b9a20e11f386ceecdb05d6848253
ekeneezeani <108833282+ekeneezeani@users.noreply.github.com>
2022-08-22 20:15:28 -0500

    Updated using Template String

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    console.log('🛑🛑🛑🛑🛑🛑 ' +  actual + ' !== ' + expected);
+    console.log(`🛑🛑🛑🛑🛑🛑 ${actual} !== ${expected}`);
   } else {
-    console.log('✅✅✅✅✅✅ ' +  actual + ' === ' + expected);
+    console.log(`✅✅✅✅✅✅ ${actual} === ${expected}`);
   }
 };
 
-assertEqual('lighthouse', 'Bootcamp');
+assertEqual('lighthouse labs', 'Bootcamp');
 assertEqual(1,1);
